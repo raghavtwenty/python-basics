9b0df7f492f30f58c9f5811431dafdb8ed6061a3
Raghav <raghavtwenty@gmail.com>
2023-04-14 07:31:28 +0530

    added python files

---
+++ ragrange.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 28 12:55:49 2019
+
+@author: User
+"""
+
+#28 AUG 2019
+#raghav 11 c
+n= int(input('enter the value'))
+for i in range (0,n+1):
+    print (i)
---
+++ ragvote.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 26 13:00:05 2019
+
+@author: User
+"""
+
+#28 AUG 2019
+#raghav 11 c
+print ('welcome to election age finder')
+age=int(input(' please enter your age:   '))
+if age>=18:
+    print('you are eligible to vote')
+    print("think twice before voting")
+    print('happy voting')
+elif age<=1:
+    print('try after 17 years or above')
+elif age<=2:
+    print('try after 16 years or above')
+elif age<=3:
+    print('try after 15 years or above')
+elif age<=4:
+    print('try after 14 years or above')
+elif age<=5:
+    print('try after 13 years or above')
+elif age<=6:
+    print('try after 12 years or above')
+elif age<=7:
+    print('try after 11 years or above')
+elif age<=8:
+    print('try after 09 years or above')
+elif age<=9:
+    print('try after 08 years or above')
+elif age<=10:
+    print('try after 07 years or above')
+elif age<=11:
+    print('try after 06 years or above')
+elif age<=12:
+    print('try after 05 years or above')
+elif age<=13:
+    print('try after 04 years or above')
+elif age<=14:
+    print('try after 03 years or above')
+elif age<=15:
+    print('try after 02 years or above') 
+elif age<=16:
+    print('try after 01 years or above')    
+else :
+    print('next year you are eligible vote')    
+print('thankyou')  
+
+
+
+
+
+  
