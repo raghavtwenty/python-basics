4c5caaa3355c5cb6dd38ff0ba022a7032652e1d3
Raghav <raghavtwenty@gmail.com>
2023-04-18 07:25:53 +0530

    added python files

---
+++ ragh.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  7 12:54:02 2019
+
+@author: raghav
+"""
+
+rollno = 1111
+name = "raghav"
+age = 16
+sport = "athletics"
+grade="b"
+print (name,age,sport,rollno)
---
+++ ragrangestr.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 28 13:11:44 2019
+
+@author: raghav
+"""
+
+n1=str(input("enter the your name:   "))
+for letter in n1:
+    print(letter)
