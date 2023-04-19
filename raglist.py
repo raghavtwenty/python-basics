23be327db686166dd6537a2fe3970eb559660a87
Raghav <raghavtwenty@gmail.com>
2023-04-19 17:23:42 +0530

    added python files

---
+++ raglist.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 30 14:10:14 2019
+
+@author: raghav
+"""
+
+# raghav
+# 30 aug 2019
+marks = [80, 40, 20, 25, 55]
+for i in marks:
+    print(i)
---
+++ ragmarks.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 19 11:26:41 2019
+
+@author: raghav
+"""
+
+phy=eval(input("marks in physics"))
+cs=eval(input("marks in cs"))
+chem=eval(input("marks in chem"))
+avg=(phy+chem+cs)/3
+print ("average marks=",avg)
