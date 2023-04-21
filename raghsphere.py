301d9496d775cefb68aed16a4fc137b402610e9c
Raghav <raghavtwenty@gmail.com>
2023-04-21 06:33:56 +0530

    added python files

---
+++ raghsphere.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 19 11:14:52 2019
+
+@author: raghav
+"""
+
+r = eval(input("enter the radius"))
+v = 4/3*math.pi*(r**3)
+print("volume of sphere=", v)
---
+++ ragusername.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 16 14:19:06 2019
+
+@author: raghav
+"""
+
+name1=input("enter the first name:  ")
+name2=input("enter thr second name: ")
+s=name1+name2
+print ("user id=", s+str(123))
