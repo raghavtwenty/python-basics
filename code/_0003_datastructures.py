0f3266dc17ed10a797cb090b84f96b66818e799b
raghavtwenty <raghavtwenty@gmail.com>
2024-07-01 21:29:59 +0530

    added datastructures

---
+++ code/_0003_datastructures.py
@@ -0,0 +1,26 @@
+# Filename: _0003_datastructures.py
+# Title: Data structures in python
+# Author: Raghava | GitHub : @raghavtwenty
+# Date Created: July 1, 2024 | Last Updated: July 1, 2024
+# Language: Python | Version: 3.12.3, 64-bit
+
+# List
+list_var = [1, 2, 3, 4, 5]
+
+# Tuple
+tuple_var = (1, 2, 3, 4, 5)
+
+# Set
+set_var = {1, 2, 3, 4, 5}
+
+# Frozenset
+frozenset_var = frozenset([1, 2, 3, 4, 5])
+
+# Dictionary
+dict_var = {'a': 1, 'b': 2, 'c': 3}
+
+# Bytes
+bytes_var = b"Hello, Bytes!"
+
+# Bytearray
+bytearray_var = bytearray([1, 2, 3, 4, 5])
