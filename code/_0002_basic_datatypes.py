c57f361a90d7c99f4e9dcbadefbbb671af9eee17
raghavtwenty <raghavtwenty@gmail.com>
2024-07-01 21:29:33 +0530

    added basic datatypes

---
+++ code/_0002_basic_datatypes.py
@@ -0,0 +1,30 @@
+# Filename: _0002_basic_datatypes.py
+# Title: Basic datatypes in python
+# Author: Raghava | GitHub : @raghavtwenty
+# Date Created: July 1, 2024 | Last Updated: July 1, 2024
+# Language: Python | Version: 3.12.3, 64-bit
+
+# = Assignment 
+
+# String
+string_var = "Hello, Python!"
+
+# Integer
+integer_var = 10
+
+# Float
+float_var = 10.5
+
+# Complex Number
+complex_var = 1 + 2j
+
+# Boolean
+boolean_var = True
+
+# None 
+data = None
+
+# Character
+ch = chr(90)
+
+
