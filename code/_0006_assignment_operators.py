469b62ecbb7d8f79fef19d5371b280d954dc389d
raghavtwenty <raghavtwenty@gmail.com>
2024-07-03 07:06:23 +0530

    added assignment operators

---
+++ code/_0006_assignment_operators.py
@@ -0,0 +1,37 @@
+"""
+Filename: _0006_assignment_operators.py
+Title: Assignment operators in python
+Author: Raghava | GitHub : @raghavtwenty
+Date Created: July 3, 2024 | Last Updated: July 3, 2024
+Language: Python | Version: 3.12.3, 64-bit
+"""
+
+a = 10
+
+# Add and assign
+a += 5      # a = a+5
+print(a) 
+
+# Subtract and assign
+a -= 3
+print(a) 
+
+# Multiply and assign
+a *= 2
+print(a)  
+
+# Divide and assign
+a /= 4
+print(a) 
+
+# Modulus and assign
+a %= 5
+print(a)  #
+
+# Exponentiation and assign
+a **= 3
+print(a) 
+
+# Floor division and assign
+a //= 2
+print(a) 
