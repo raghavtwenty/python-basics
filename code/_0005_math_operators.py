2b2dbb960604b6dd7d8a35ef39a2352f062e46c5
raghavtwenty <raghavtwenty@gmail.com>
2024-07-03 07:06:10 +0530

    added math operators

---
+++ code/_0005_math_operators.py
@@ -0,0 +1,37 @@
+"""
+Filename: _0005_math_operators.py
+Title: Mathematical operators in python
+Author: Raghava | GitHub : @raghavtwenty
+Date Created: July 3, 2024 | Last Updated: July 3, 2024
+Language: Python | Version: 3.12.3, 64-bit
+"""
+
+# Addition
+a = 10
+b = 20
+result = a + b
+print("Addition:", result)  
+
+# Subtraction
+result = b - a
+print("Subtraction:", result)  
+
+# Multiplication
+result = a * b
+print("Multiplication:", result)  
+
+# Division
+result = b / a
+print("Division:", result)  
+
+# Modulus
+result = b % a
+print("Modulus:", result)  
+
+# Exponentiation
+result = a ** 2
+print("Exponentiation:", result)  
+
+# Floor Division
+result = b // a
+print("Floor Division:", result)  
