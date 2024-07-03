b9a7d26e6fce71a4ba43dcbf8d3badfce00d0d24
raghavtwenty <raghavtwenty@gmail.com>
2024-07-03 07:05:55 +0530

    added comments

---
+++ code/_0004_comments.py
@@ -0,0 +1,27 @@
+"""
+Filename: _0004_operators.py
+Title: Comments in python
+Author: Raghava | GitHub : @raghavtwenty
+Date Created: July 3, 2024 | Last Updated: July 3, 2024
+Language: Python | Version: 3.12.3, 64-bit
+"""
+
+"""
+docstring always at the top of file/function
+"""
+
+# Single line comment with #
+
+"""
+Multiline comments with triple(double) quotes "
+"""
+
+'''
+Multiline comments with triple(single) quotes '
+'''
+
+data = 10   # data is a variable
+"""
+display the value in data
+"""
+print(data)
