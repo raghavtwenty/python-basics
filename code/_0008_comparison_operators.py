72c4f473aafb861de8f9f0a8753da8a5489da157
raghavtwenty <raghavtwenty@gmail.com>
2024-07-04 11:24:29 +0530

    added comparison operators

---
+++ code/_0008_comparison_operators.py
@@ -0,0 +1,28 @@
+"""
+Filename: _0008_comparison_operators.py
+Title: Logical operators in python
+Author: Raghava | GitHub : @raghavtwenty
+Date Created: July 4, 2024 | Last Updated: July 4, 2024
+Language: Python | Version: 3.12.3, 64-bit
+"""
+
+a = 10
+b = 20
+
+# Equal
+print(a == b)  
+
+# Not equal
+print(a != b)  
+
+# Greater than
+print(a > b)  
+
+# Less than
+print(a < b) 
+
+# Greater than or equal to
+print(a >= b)  
+
+# Less than or equal to
+print(a <= b)  
