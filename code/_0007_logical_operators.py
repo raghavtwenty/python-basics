648bb156d6da68c390c2ecc4cb1e632848f40ddc
raghavtwenty <raghavtwenty@gmail.com>
2024-07-04 11:23:44 +0530

    added logical operators

---
+++ code/_0007_logical_operators.py
@@ -0,0 +1,19 @@
+"""
+Filename: _0007_logical_operators.py
+Title: Logical operators in python
+Author: Raghava | GitHub : @raghavtwenty
+Date Created: July 4, 2024 | Last Updated: July 4, 2024
+Language: Python | Version: 3.12.3, 64-bit
+"""
+
+a = True
+b = False
+
+# Logical AND
+print(a and b)  
+
+# Logical OR
+print(a or b)  
+
+# Logical NOT
+print(not a)  
